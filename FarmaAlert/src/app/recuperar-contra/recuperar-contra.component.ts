e376b53c901b08a82af7f9ef1d8fcc4485de0d50
JuanCruz1103 <juanantoniocr1103@gmail.com>
2025-03-10 09:50:07 -0700

    Proyecto integrador

---
+++ FarmaAlert/src/app/recuperar-contra/recuperar-contra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperar-contra',
+  imports: [],
+  templateUrl: './recuperar-contra.component.html',
+  styleUrl: './recuperar-contra.component.css'
+})
+export class RecuperarContraComponent {
+
+}
---
+++ FarmaAlert/src/app/tabla-trabajadores/tabla-trabajadores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-trabajadores',
+  imports: [],
+  templateUrl: './tabla-trabajadores.component.html',
+  styleUrl: './tabla-trabajadores.component.css'
+})
+export class TablaTrabajadoresComponent {
+
+}
