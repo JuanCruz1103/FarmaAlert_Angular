b75fec044d96523fef5a5203912c20910dd2b127
juanpablo362 <juanpablomoralesrivera5@gmail.com>
2025-03-21 00:02:21 -0700

    feth(trabajadores):Correccion registros

--- FarmaAlert/src/app/tabla-trabajadores/tabla-trabajadores.component.ts
+++ FarmaAlert/src/app/tabla-trabajadores/tabla-trabajadores.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabla-trabajadores',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './tabla-trabajadores.component.html',
-  styleUrl: './tabla-trabajadores.component.css'
+  styleUrl: './tabla-trabajadores.component.css',
 })
 export class TablaTrabajadoresComponent {
   // Lista de trabajadores
@@ -14,7 +14,7 @@ export class TablaTrabajadoresComponent {
     { id: 1, nombre: 'Carlos Pérez', edad: 34, paciente: 'María Teresa' },
     { id: 2, nombre: 'Luisa Gómez', edad: 28, paciente: 'José Fernández' },
     { id: 3, nombre: 'Pedro Ramírez', edad: 40, paciente: 'Ana López' },
-    { id: 4, nombre: 'Sofía Martínez', edad: 32, paciente: 'Roberto Díaz' }
+    { id: 4, nombre: 'Sofía Martínez', edad: 32, paciente: 'Roberto Díaz' },
   ];
   agregar() {
     alert('Agregar trabajador');
@@ -28,7 +28,7 @@ export class TablaTrabajadoresComponent {
   // Función para eliminar un trabajador
   eliminar(id: number) {
     if (confirm('¿Estás seguro de eliminar este trabajador?')) {
-      this.trabajadores = this.trabajadores.filter(t => t.id !== id);
+      this.trabajadores = this.trabajadores.filter((t) => t.id !== id);
     }
   }
 }
