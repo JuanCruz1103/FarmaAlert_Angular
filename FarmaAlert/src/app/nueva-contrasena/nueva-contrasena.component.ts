fbe0d53804c99cfc324cfb2fd97be2b8a1e8378e
PC-MAESTRO <PC-MAESTRO@DESKTOP-7N4A5CP>
2025-04-07 20:12:27 -0700

    codigo de nueva contraseña

--- FarmaAlert/src/app/nueva-contrasena/nueva-contrasena.component.ts
+++ FarmaAlert/src/app/nueva-contrasena/nueva-contrasena.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './nueva-contrasena.component.css'
 })
 export class NuevaContrasenaComponent {
+  newPassword: string = '';
+  confirmPassword: string = '';
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  confirm() {
+    if (this.newPassword === this.confirmPassword && this.newPassword.length > 0) {
+      alert('Contraseña cambiada correctamente');
+    } else {
+      alert('Las contraseñas no coinciden o están vacías');
+    }
+  }
 }
